71ca6af34cfb028073bf5a3bf67b5b9f405ecac6
Kostyantyn Proskuryakov <kostyapro@gmail.com>
2013-11-06 10:18:12 -0500

    Because whatever

--- IDT-OHS1-Winter-2013/src/org/ohs1/winter2013/Dummy.java
+++ IDT-OHS1-Winter-2013/src/org/ohs1/winter2013/Dummy.java
@@ -1,7 +1,9 @@
 package org.ohs1.winter2013;
 
 public class Dummy {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		
+		System.out.println("lol");
+		
 	}
 }
