9efa7b58df424c1bf71d1322253dc194bd40c238
Kostyantyn Proskuryakov <kostyapro@gmail.com>
2013-11-07 19:24:51 -0500

    Added a small javadoc message. Ian, you'll probably need to set up javadoc in eclipse with this.

--- IDT-OHS1-Winter-2013/src/org/ohs1/winter2013/Dummy.java
+++ IDT-OHS1-Winter-2013/src/org/ohs1/winter2013/Dummy.java
@@ -1,5 +1,13 @@
 package org.ohs1.winter2013;
 
+
+/**
+ * This dummy class is an example of javadoc commenting. The idea is that we'd just use
+ * this space for the description of the class below this. @author is required and so 
+ * is @version. 
+ * 
+ * @author KProskuryakov
+ */
 public class Dummy {
 	public static void main(String[] args) {
 		
